baffc6f0bad014f85884db91ee4299ecfaf6c58d
Nikolay Andonov <nikolay.andonov@mentormate.com>
2018-12-25 11:01:26 +0200

    configuration for using the heroku's postgreSQL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,32 +11,39 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try services.register(FluentPostgreSQLProvider())
     try services.register(LeafProvider())
     try services.register(AuthenticationProvider())
-
+    
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     middlewares.use(SessionsMiddleware.self)
-
+    
+    let postgresqlConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        postgresqlConfig = PostgreSQLDatabaseConfig(url: url)!
+    }
+    else {
+        postgresqlConfig = PostgreSQLDatabaseConfig(
+            serverAddress: PostgreSQLConnection.ServerAddress.tcp(hostname: "baasu.db.elephantsql.com", port: 5432),
+            username: "vdwegziv",
+            database: "vdwegziv",
+            password: "WSDEkQDZdenEafNbJGoyCc80N02Jq391"
+        )
+    }
+    
     // Configure a PostgreSQL database
-    let postgresqlConfig = PostgreSQLDatabaseConfig(
-        serverAddress: PostgreSQLConnection.ServerAddress.tcp(hostname: "baasu.db.elephantsql.com", port: 5432),
-        username: "vdwegziv",
-        database: "vdwegziv",
-        password: "WSDEkQDZdenEafNbJGoyCc80N02Jq391"
-    )
     let postgresqlDatabase = PostgreSQLDatabase(config: postgresqlConfig)
     
     var databases = DatabasesConfig()
     databases.add(database: postgresqlDatabase, as: .psql)
     services.register(databases)
-
-
+    
+    
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .psql)
